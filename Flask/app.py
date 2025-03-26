0f0c7cab1c3cc180ae4247a8a64e4ed9df6e8cb2
Ayush Sawant <ayushsawant2005@gmail.com>
2025-03-26 21:36:45 +0530

    AppCreated

--- Flask/app.py
+++ Flask/app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home(): 
     return render_template("index.html")
+@app.route('/scanner', methods=['GET', 'POST'])
+def about(): 
+    return render_template("scanner.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
